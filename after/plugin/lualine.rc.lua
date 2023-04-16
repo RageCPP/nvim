9858056dd7313e3a4b0aee35fa276c4503ec6c59
ragecpp <mathematicscpu@gmail.com>
2023-04-16 09:47:07 +0800

    update

--- after/plugin/lualine.rc.lua
+++ after/plugin/lualine.rc.lua
@@ -4,55 +4,82 @@ local icon_loaded, icon = pcall(require, "nvim-web-devicons")
 if (not status) then return end
 if (not icon_loaded) then return end
 
-lualine.setup {
-    options = {
-    icons_enabled = true,
-    theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
+local colors = {
+    active_fg   = "#ffc773",
+    active_bg   = "#5D2099",
+    fg          = "#808080",
+    bg          = "transparent",
+}
+
+-- config
+local config = {
+  options = {
+    component_separators = '',
+    section_separators = '',
+    theme = {
+      normal = { c = { fg = colors.fg, bg = colors.bg } },
+     -- inactive = { c = { fg = colors.active_fg, bg = colors.active_bg } }
     },
-    ignore_focus = {},
-    always_divide_middle = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-    }
   },
   sections = {
-    lualine_a = {{
-      'buffers',
-      mode = 1,
-      icons_enabled = false,
-    }},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {{
-      'filename',
-      path = 1,
-      shorting_target = 20
-    }},
-    lualine_x = {{
-      'filetype',
-      colored = true,
-      icon = { align = 'left' },
-    }},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
+    -- these are to remove the defaults
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
     lualine_y = {},
-    lualine_z = {}
+    lualine_z = {},
+    -- These will be filled later
+    lualine_c = {},
+    lualine_x = {},
   },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
+  inactive_sections = {
+    -- these are to remove the defaults
+    lualine_a = {},
+    lualine_b = {},
+    lualine_y = {},
+    lualine_z = {},
+    lualine_c = {},
+    lualine_x = {},
+  },
+  refresh = {
+    statusline = 1000,
+    tabline = 1000,
+    winbar = 1000,
+  }
 }
+
+local function insert(section, widget)
+  table.insert(section, widget)
+end
+
+insert(config.sections.lualine_a, {
+  'branch'
+})
+
+insert(config.sections.lualine_a, {
+  'diff',
+  colored = true
+})
+
+insert(config.sections.lualine_b, { 'diagnostics' })
+
+insert(config.sections.lualine_x, {
+  'buffers',
+  mode = 0,
+  icons_enabled = false,
+  buffers_color = {
+    active = { fg = colors.active_fg,  bg = colors.active_bg },
+    normal = { fg = colors.fg, bg = colors.bg }
+  }
+})
+
+insert(config.sections.lualine_x, {
+  'filetype',
+  colored = true,
+  icon = { align = 'left' }
+})
+
+insert(config.sections.lualine_y, {'progress'})
+
+insert(config.sections.lualine_z, {'location'})
+
+lualine.setup(config)
--- after/plugin/telescope.rc.lua
+++ after/plugin/telescope.rc.lua
@@ -23,11 +23,11 @@ local function telescope_buffer_dir()
   return vim.fn.expand('%:p:h')
 end
 
-vim.keymap.set('n', ';f',function() builtin.find_files({no_ignore = false,hidden = true}) end)
+vim.keymap.set('n', ';f', function() builtin.find_files({no_ignore = true,hidden = true}) end)
 vim.keymap.set('n', ';s', builtin.live_grep) -- 在您当前的工作目录中搜索一个字符串，并在您键入时实时获得结果，忽略.gitignore
 vim.keymap.set('n', ';b', builtin.buffers)
 vim.keymap.set('n', ';h', builtin.help_tags)
 vim.keymap.set('n', ';r', builtin.resume) -- 恢复上次打开telescope的状态
 
-vim.keymap.set('n', ';c', builtin.diagnostics) -- 列出所有打开的缓冲区或特定缓冲区的诊断。使用bufnr=0当前缓冲区的选项。
+voim.keymap.set('n', ';n', function() builtin.diagnostics({bufnr = 0}) end) -- 列出所有打开的缓冲区或特定缓冲区的诊断。使用bufnr=0当前缓冲区的选项。
 vim.keymap.set('n', ';d', function() builtin.lsp_definitions({jumptype = "never"}) end)
